8beeaac36c35e506913fb35c4c6262f968dac4d7
Amy Oulton <amyeoulton@gmail.com>
2020-04-30 14:00:45 -0700

    generated seeds file

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+    validates(:title, presence: true, uniqueness: true)
+    validates(
+        :body,
+        presence: true,
+        length: { minimum: 50 }
+    )
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Post.delete_all
+
+75.times do 
+    created_at = Faker::Date.backward(days: 365 * 2)
+    Post.create(
+        title: Faker::Hipster.unique.sentence,
+        body: Faker::Hipster.paragraph_by_chars(characters: 256, supplemental: false),
+        created_at: created_at,
+        updated_at: created_at
+    )
+end
